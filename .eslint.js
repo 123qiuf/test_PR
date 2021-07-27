079d4446f3aaafd8872f1c6811d90fed88bc25b7
123qiuf <1014241919@qq.com>
2021-07-27 15:57:39 +0800

    Add files via upload

---
+++ .eslint.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //　添加插件
+  plugins: ["vue"],
+  rules: {
+    "no-console": "off"
+  }
+}
+//package.json已配置,已覆盖此文件 eslintConfig
---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: ['@vue/app'],
+  plugins: [
+    // [
+    //   'import',
+    //   {
+    //     libraryName: 'element-ui',
+    //     style: (name) => {
+    //       return `element-ui/lib/theme-chalk/${name.substr(name.lastIndexOf('/') + 1)}.css`;
+    //     }
+    //   },
+    //   'element-ui'
+    // ]
+  ]
+};
